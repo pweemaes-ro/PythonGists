c1d8de497227a08de2760e1870a5a71051af2c17
pweemaes-ro <zappamotel@quicknet.nl>
2024-02-28 22:55:52 +0100

    After major modification

--- MazeProblems/MostRightTurns.py
+++ MazeProblems/MostRightTurns.py
@@ -3,9 +3,10 @@ from typing import TypeAlias
 
 
 Location: TypeAlias = tuple[int, int]
-Path: TypeAlias = list[Location]
+Locations: TypeAlias = list[Location]
+Path: TypeAlias = dict[Location, int]
 NrRightTurns: TypeAlias = int
-PathInfo: TypeAlias = tuple[NrRightTurns, Path]
+PathInfo: TypeAlias = tuple[NrRightTurns, Locations]
 Maze = list[str]
 
 __all_path_infos: list[PathInfo] = []
@@ -43,26 +44,27 @@ def is_valid_move(maze: Maze, destination: Location) -> bool:
 	        maze[y][x] != '#')
 
 
-def count_right_turns(path: Path) -> int:
-	"""Return the nr of right turns in the given path."""
+def count_right_turns(locations: Locations) -> int:
+	"""Return the nr of right turns in the given ordered path."""
 	
-	return sum(any([prev == (x + 1, y) and dest == (x, y - 1),
-	                prev == (x, y + 1) and dest == (x + 1, y),
-	                prev == (x - 1, y) and dest == (x, y + 1),
-	                prev == (x, y - 1) and dest == (x - 1, y)])
-	           for prev, (x, y), dest in zip(path, path[1:], path[2:]))
+	return sum(
+		any([prev == (x + 1, y) and dest == (x, y - 1),
+		     prev == (x, y + 1) and dest == (x + 1, y),
+		     prev == (x - 1, y) and dest == (x, y + 1),
+		     prev == (x, y - 1) and dest == (x - 1, y)])
+		for prev, (x, y), dest in zip(locations, locations[1:], locations[2:]))
 
 
 def is_valid_path(path: Path) -> bool:
 	"""Return True if all required parts are in the path, else False."""
-	
+
 	for r in __required:
 		try:
-			index = path.index(r[0])
+			index = path[r[0]]
 			for i in range(1, len(r)):
-				if r[i] != path[index + i]:
+				if path[r[i]] != index + i:
 					return False
-		except ValueError:
+		except KeyError:
 			return False
 	return True
 
@@ -74,26 +76,27 @@ def find_all_paths(maze: Maze,
 	""" Finds all possible paths from start to finish in the maze."""
 	
 	if path is None:
-		path = []
+		path = dict()
 	
 	# Append current start position to the path
-	path.append(start)
+	path[start] = len(path.items()) + 1
 	
 	x, y = start
 	if (x, y) == finish:
 		if is_valid_path(path):
-			__all_path_infos.append((count_right_turns(path), path.copy()))
+			locations = [k for (v, k) in
+			             sorted([(v, k) for (k, v) in path.items()])]
+			__all_path_infos.append((count_right_turns(locations), locations))
 	else:
 		for destination in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
-			# todo: find more efficient way to check if destination in path...
 			if is_valid_move(maze, destination) and destination not in path:
 				find_all_paths(maze, destination, finish, path)
 	
 	# Remove current position from the path to backtrack
-	path.pop()
+	del path[(x, y)]
 
 
-def print_path(maze: Maze, path: Path) -> None:
+def print_path(maze: Maze, locations: Locations) -> None:
 	"""Print the path"""
 	
 	printable_matrix: list[list[str]] = []
@@ -105,7 +108,7 @@ def print_path(maze: Maze, path: Path) -> None:
 		printable_matrix.append(line)
 	
 	# Overwrite open spaces that are on path.
-	for idx, (x, y) in enumerate(path, start=1):
+	for idx, (x, y) in enumerate(locations, start=1):
 		printable_matrix[y][x] = f"{idx:3d}"
 	
 	"""Finally, print the (per line) joined strings in the matrix"""
