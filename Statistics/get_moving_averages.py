bfa6880fe7fed70174d6db5c113751baac1b0bcf
pweemaes-ro <zappamotel@quicknet.nl>
2024-03-22 09:05:05 +0100

    get_moving_averages now supports a flag (as param) indicating whether 'incomplete windows' should be included.

--- Statistics/get_moving_averages.py
+++ Statistics/get_moving_averages.py
@@ -2,25 +2,41 @@
 
 from collections import deque
 from collections.abc import Sequence
-from typing import TypeVar
+from typing import TypeAlias, Union
 
-Number = TypeVar("Number", int, float)
+Number: TypeAlias = Union[int, float]
 
 
-def get_moving_averages(data: Sequence[Number], window_size: int) \
-	-> Sequence[float]:
-	"""Return a list of moving averages of data. The first ma value is
-	calculated over the first window_size datapoints, the last over the last
-	window_size datapoints. Therefore the length of the returned list is length
-	data - window_size + 1"""
+def get_moving_averages(data: Sequence[Number],
+                        window_size: int,
+                        include_incomplete: bool = False) -> Sequence[float]:
+	"""Return a list of moving averages of data. If include_incomplete is
+	False, the first ma value is calculated over the first window_size
+	datapoints; if include_incomplete is True, averages for windows of lengths
+	1 to window_size - 1 are included. The last average is calculated over the
+	last window_size datapoints."""
 	
-	ma = []
+	if not isinstance(window_size, int):
+		
+		raise TypeError("Window_size must be an int.")
+		
+	if window_size < 1:
 	
-	if window_size > 0:
-		window = deque(data[:window_size - 1], maxlen=window_size)
-
-		for value in data[window_size - 1:]:
+		raise ValueError("Window_size must be a positive int.)")
+	
+	else:
+	
+		ma = []
+	
+		window: deque[Number] = deque(maxlen=window_size)
+		offset = 0
+		
+		if not include_incomplete:
+			offset = window_size - 1
+			window.extend(data[:window_size - 1])
+	
+		for value in data[offset:]:
 			window.append(value)
-			ma.append(sum(window) / window_size)
+			ma.append(sum(window) / len(window))
 	
-	return ma
+		return ma
